17713e706fb515f0f722c91b209c74cc15ff2fed
Thomas L'Hostis <thomas.lhostis@gmail.com>
2020-12-31 11:37:37 +0100

    Initialisation

---
+++ src/main/kotlin/fr/thomaslhostis/architecturehexagonale/ArchitectureHexagonaleApplication.kt
@@ -0,0 +1,11 @@
+package fr.thomaslhostis.architecturehexagonale
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchitectureHexagonaleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArchitectureHexagonaleApplication>(*args)
+}
